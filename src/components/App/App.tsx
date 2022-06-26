167975aea637bb47ae3f4b0ac8a3a28ba72f2ca5
Oliver Dunk <oliver@oliverdunk.com>
2022-06-26 21:23:09 +0100

    Add header with doc links

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,8 +7,43 @@ export function App() {
   return (
     <StateProvider>
       <div className={styles.app}>
-        <Sidebar />
-        <Editor />
+        <nav className={styles.docs}>
+          <ul>
+            <li>
+              <a
+                href="https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions"
+                target="_blank"
+              >
+                MDN
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://developer.chrome.com/docs/extensions/"
+                target="_blank"
+              >
+                Chrome Developers
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://developer.apple.com/documentation/safariservices/safari_web_extensions"
+                target="_blank"
+              >
+                Apple Developer Docs
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/w3c/webextensions" target="_blank">
+                WECG
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.main}>
+          <Sidebar />
+          <Editor />
+        </div>
       </div>
     </StateProvider>
   );
